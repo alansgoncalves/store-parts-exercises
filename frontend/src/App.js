2da2d67fc32d1bf01b20365bd8d64091c65d41d5
alansgoncalves <vekio.soft@gmail.com>
2022-09-12 18:44:56 -0300

    feat: create input fo searching terms

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,11 @@
 import "./App.css";
 import MainPage from "./pages/MainPage";
-import PartsPage from "./pages/PartsPage";
+// import PartsPage from "./pages/PartsPage";
 
 function App() {
 	return (
 		<div className="App">
-			<h2>Teste</h2>
-			<PartsPage />
+			{/* <PartsPage /> */}
 			<MainPage />
 		</div>
 	);
--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
+import { Input } from "@chakra-ui/react";
 
 const MainPage = () => {
 	const [products, setProducts] = useState([]);
+	const [searchTerm, setSearchTerm] = useState("");
 
 	useEffect(() => {
 		(async () => {
@@ -13,14 +15,34 @@ const MainPage = () => {
 
 	return (
 		<div>
+			<Input
+				placeholder="Search"
+				w="30%"
+				m="auto"
+				onChange={(event) => {
+					setSearchTerm(event.target.value);
+				}}
+			/>
+
 			<ul>
-				{products.map(({ name, price, type }) => (
-					<li>
-						{name}
-						{type}
-						{price}
-					</li>
-				))}
+				{products
+					.filter((value) => {
+						if (searchTerm === "") {
+							return value;
+						} else if (
+							value.name.toLowerCase().includes(searchTerm.toLowerCase())
+						) {
+							return value;
+						}
+						return false;
+					})
+					.map(({ name, price, type }) => (
+						<li key={name}>
+							{name}
+							{type}
+							{price}
+						</li>
+					))}
 			</ul>
 		</div>
 	);
